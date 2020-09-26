47716194da7e02d20115d4abd050ab359d99709c
jar8-stack <bioshock450226@hotmsil.com>
2020-09-25 23:07:14 -0500

    game model agregado

---
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/GameModel.kt
@@ -0,0 +1,7 @@
+package com.example.proyecto_1_aguero_castillo_canul_serrano
+
+import androidx.lifecycle.ViewModel
+
+class GameModel : ViewModel() {
+
+}
--- app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/MainActivity.kt
@@ -3,7 +3,14 @@ package com.example.proyecto_1_aguero_castillo_canul_serrano
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
+
+    private val model: GameModel
+        get() {
+            TODO()
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
