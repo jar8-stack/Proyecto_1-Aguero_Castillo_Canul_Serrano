30968bbfd83fcd73b0f48271295b8748d842d4f8
Manuel Canul <manuel.canul109@gmail.com>
2020-09-27 14:30:23 -0500

    Se actualizo a SettingsActivity

--- app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/SettingsActivity.kt
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/SettingsActivity.kt
@@ -1,9 +1,11 @@
 package com.example.proyecto_1_aguero_castillo_canul_serrano
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Settings : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
