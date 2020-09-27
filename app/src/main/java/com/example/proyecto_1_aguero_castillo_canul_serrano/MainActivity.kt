5ca21c0b645889a61bc68d877593b57ac5e69edf
Ana Castillo <luna_acce@hotmail.com>
2020-09-26 21:33:31 -0500

    Botones de juego y opciones

--- app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.proyecto_1_aguero_castillo_canul_serrano
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +17,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSettings.setOnClickListener {
+            val intent:Intent = Intent(this, Settings::class.java)
+            startActivity(intent)
+        }
     }
 }
