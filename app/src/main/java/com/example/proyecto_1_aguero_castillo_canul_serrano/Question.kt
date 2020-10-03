a1cfa03c09b192c87d500f6e6f774fdd189a2239
Manuel Canul <manuel.canul109@gmail.com>
2020-10-03 12:10:52 -0500

    Clases de ayuda

---
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/Question.kt
@@ -0,0 +1,3 @@
+package com.example.proyecto_1_aguero_castillo_canul_serrano
+
+data class Question(val strResId:Int,val answer:Boolean,var answered:Boolean)
---
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/ThemeQuestion.kt
@@ -0,0 +1,3 @@
+package com.example.proyecto_1_aguero_castillo_canul_serrano
+
+data class ThemeQuestion(val strResId: Int, var answers:List<Int> , var correctanswer:Int, var category: String , var answered:Boolean)
