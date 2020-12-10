3edc9f502817e3074da81790a41b820601b7346f
ManuelCanul109 <manuel.canul109@gmail.com>
2020-12-10 01:48:00 -0500

    Creacion funciones userDao

--- app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/db/UserDao.kt
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/db/UserDao.kt
@@ -1,6 +1,7 @@
 package com.example.proyecto_1_aguero_castillo_canul_serrano.db
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
@@ -13,4 +14,10 @@ interface UserDao {
 
     @Query("INSERT INTO usuario(nombre_usuario, score_usuario, id_configuration) VALUES(:userName, :score, :id_config)")
     fun insertUser(userName:String,score:Int,id_config:Int)
+
+    @Query("SELECT id_usuario from usuario order by id_usuario DESC limit 1")
+    fun traerUltimoIdUsuarios() : Int
+
+    @Insert
+    fun insertUser(user: User)
 }
