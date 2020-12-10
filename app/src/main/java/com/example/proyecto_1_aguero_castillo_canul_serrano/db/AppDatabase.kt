cbcb69e77a970171333f02b0f2bb7192bdb4b7a6
ManuelCanul109 <manuel.canul109@gmail.com>
2020-12-10 01:45:33 -0500

    Agregacion de los demas DAO

--- app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/db/AppDatabase.kt
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/db/AppDatabase.kt
@@ -3,9 +3,10 @@ package com.example.proyecto_1_aguero_castillo_canul_serrano.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = arrayOf(Theme::class, User::class, configuration_themes::class, configurations::class, question_answers::class, Questions::class), version = 1)
+@Database(entities = arrayOf(Theme::class, User::class, configuration_themes::class, Configuration::class, question_answers::class, Questions::class), version = 1)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun themeDao(): ThemesDao
-
     abstract fun userDao(): UserDao
+    abstract fun configurationDao(): ConfigurationDao
+    abstract fun ConfigurationThemesDAO(): ConfigurationThemesDAO
 }
