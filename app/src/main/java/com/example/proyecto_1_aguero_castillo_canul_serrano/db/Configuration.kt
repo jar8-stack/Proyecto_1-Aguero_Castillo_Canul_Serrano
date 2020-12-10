7f041d786b5d9cd080751dbf15d460b778ce9333
ManuelCanul109 <manuel.canul109@gmail.com>
2020-12-10 01:45:49 -0500

    Se creo del modelo de configuracion

---
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/db/Configuration.kt
@@ -0,0 +1,14 @@
+package com.example.proyecto_1_aguero_castillo_canul_serrano.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "configurations")
+data class Configuration(
+    @PrimaryKey @ColumnInfo(name = "id_configuration") val id_configuration: Int,
+    @ColumnInfo(name = "number_questions") var number_questions: Int,
+    @ColumnInfo(name = "dificulty") var dificulty: String,
+    @ColumnInfo(name = "number_tracks") var number_tracks: Int,
+    @ColumnInfo(name = "tracks_enabled") var tracks_enabled: Boolean
+)
